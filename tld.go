6f1052c40627a1444b1f864138309ebba37ab0aa
Ethan Olsen <131eolsen@gmail.com>
2025-01-27 03:52:18 -0800

    feat: add tld list tmp saving

--- tld.go
+++ tld.go
@@ -4,10 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strings"
+	"time"
+
+	"github.com/o2dependent/go-scrape/utils"
 )
 
 var tldListURL = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
+var tldListTempFileName = "scrapper-tld-list"
 
 func failedGetTLDsMessage() {
 	fmt.Println("failed to get TLD list")
@@ -15,9 +20,15 @@ func failedGetTLDsMessage() {
 }
 
 func getTLDs() []string {
-	tldList := []string{}
+	// check temp
+	tmpList, rerun := getTempTLD()
+	if !rerun && len(tmpList) > 0 {
+		fmt.Println("using cached TLD list")
+		return tmpList
+	}
 
-	// TODO: check tmp
+	// fetch tld list
+	tldList := []string{}
 
 	req, err := http.NewRequest(http.MethodGet, tldListURL, nil)
 	if err != nil {
@@ -44,8 +55,51 @@ func getTLDs() []string {
 	}
 
 	tldList = strings.Split(resBodyStr, "\n")
+
 	// TODO: store in tmp
+	createTempTLD(resBody)
+
 	tldList = tldList[1:]
 
 	return tldList
+}
+
+func createTempTLD(body []byte) {
+	dir := utils.GetTempDir()
+	if dir == "" {
+		return
+	}
+
+	f, err := os.Create(dir + "/" + tldListTempFileName)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	f.Write(body)
+}
+
+func getTempTLD() (tldList []string, rerun bool) {
+	rerun = true
+	dir := utils.GetTempDir()
+	if dir == "" {
+		return tldList, rerun
+	}
+
+	f, err := os.ReadFile(dir + "/" + tldListTempFileName)
+	if err != nil {
+		return tldList, rerun
+	}
+	contents := string(f)
+	tldList = strings.Split(contents, "\n")
+
+	dateStr := strings.Replace(strings.Split(tldList[0], ",")[1], " Last Updated ", "", -1)
+	// dateStr := "Mon Jan 1 15:04:05 2025 MST"
+	layout := "Mon Jan 1 15:04:05 2006 MST"
+	date, err := time.Parse(layout, dateStr)
+	if err == nil && date.Add(time.Hour*27*7).Compare(time.Now()) == -1 {
+		return tldList, rerun
+	}
+	rerun = false
+
+	return tldList[1:], rerun
 }
